3dd65f5aefd610e16d7cb5182201fab9340b6e5e
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-23 14:25:32 +0000

    add switch file and the sipicifiers

---
+++ switch.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * spicifier - spisifie the character.
+ * @format: format.
+ * @args: string to print.
+ * @len: length of the string.
+ * Return: return length.
+ */
+
+int spicifier(const char *format, va_list args, int len)
+{
+	switch (*format)
+	{
+		case 'c':
+			len = print_c(args);
+			break;
+		case 's':
+			len = print_s(args);
+			break;
+		case '%':
+			write(1, '%', 1);
+			break;
+		default:
+			break;
+	}
+	return (len);
+}
