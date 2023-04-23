7ade8accc1eadd6bdb3cf2b8ee7ac23e70b13953
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-23 21:21:00 +0100

    print (null)

--- print_s.c
+++ print_s.c
@@ -11,6 +11,12 @@ int print_s(va_list args)
 	char *str = va_arg(args, char *);
 	int len = 0;
 
+	if (str == NULL || str == 0)
+		return (print_string("(null)", count));
+
+	if (*str == '\0')
+		return (print_string("(null)", count));
+
 	while (str[len])
 		len++;
 
