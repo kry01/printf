469776642f8b97bec351ff962d696912c66632c0
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-23 17:36:16 +0100

    test

--- switch.c
+++ switch.c
@@ -14,12 +14,14 @@ int specifier(const char *format, va_list args, int len)
 	{
 		case 'c':
 			len = print_c(args);
+			len++;
 			break;
 		case 's':
 			len = print_s(args);
 			break;
 		case '%':
 			write(1, "%", 1);
+			len++;
 			break;
 		default:
 			break;
