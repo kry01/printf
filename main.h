902d2f01e9389c5b828fe066134defad4e3bcfd4
Hamza Kazdari <kazdari.hamza@gmail.com>
2023-04-23 14:36:54 +0100

    added print_c and print_s prototypes and libraries

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
 
 #endif
